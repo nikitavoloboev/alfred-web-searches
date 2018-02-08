e34f02afaefbe46ca7501db4608e4642e165975b
Nikita Voloboev <nikita.voloboev@icloud.com>
2018-02-09 00:43:55 +0100

    Move to kingpin

--- main.go
+++ main.go
@@ -1,154 +1,83 @@
 package main
 
 import (
-	"encoding/csv"
-	"log"
-	"os"
-	"os/exec"
-	"regexp"
-	"strings"
+	"fmt"
 
+	"github.com/alecthomas/kingpin"
 	"github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/update"
-	"github.com/docopt/docopt-go"
 )
-
-// Name of the background job that checks for updates
-const updateJobName = "checkForUpdate"
-
-var usage = `alfred-web-searches [search|check] [<query>]
-
-Search through any website on Earth.
-
-Usage:
-	alfred-web-searches search [<query>]
-	alfred-web-searches check
-    alfred-web-searches -h
-
-Options:
-    -h, --help    Show this message and exit.
-`
 
 var (
 	// Icons
-	iconAvailable = &aw.Icon{Value: "icons/update.png"}
-	redditIcon    = &aw.Icon{Value: "icons/reddit.png"}
-	githubIcon    = &aw.Icon{Value: "icons/github.png"}
-	translateIcon = &aw.Icon{Value: "icons/translate.png"}
-	forumsIcon    = &aw.Icon{Value: "icons/forums.png"}
-	stackIcon     = &aw.Icon{Value: "icons/stack.png"}
-	docIcon       = &aw.Icon{Value: "icons/doc.png"}
+	updateAvailable = &aw.Icon{Value: "icons/update-available.png"}
+	redditIcon      = &aw.Icon{Value: "icons/reddit.png"}
+	githubIcon      = &aw.Icon{Value: "icons/github.png"}
+	translateIcon   = &aw.Icon{Value: "icons/translate.png"}
+	forumsIcon      = &aw.Icon{Value: "icons/forums.png"}
+	stackIcon       = &aw.Icon{Value: "icons/stack.png"}
+	docIcon         = &aw.Icon{Value: "icons/doc.png"}
+
+	// Kingpin and script options
+	app *kingpin.Application
+
+	// Application commands
+	searchCmd *kingpin.CmdClause
+	updateCmd *kingpin.CmdClause
+
+	query string
 
 	repo = "nikitavoloboev/alfred-web-searches"
-	wf   *aw.Workflow
+
+	// Workflow stuff
+	wf *aw.Workflow
 )
 
+// Mostly sets up kingpin commands
 func init() {
-	wf = aw.New(update.GitHub(repo))
+	wf = aw.New(update.GitHub(repo), aw.HelpURL(repo+"/issues"))
+
+	app = kingpin.New("ask", "Search websites.")
+
+	// Update command
+	updateCmd = app.Command("update", "Check for new workflow version.").Alias("u")
+
+	// Commands using query
+	searchCmd = app.Command("search", "Search websites.").Alias("s")
+
+	// Common options
+	for _, cmd := range []*kingpin.CmdClause{
+		searchCmd,
+	} {
+		cmd.Flag("query", "Search query.").Short('q').StringVar(&query)
+	}
 }
 
 func run() {
-
-	// Pass wf.Args() to docopt because our update logic relies on
-	// AwGo's magic actions.
-	args, _ := docopt.Parse(usage, wf.Args(), true, wf.Version(), false, true)
-
-	// Alternate action: get available releases from remote
-	if args["check"] != false {
-		wf.TextErrors = true
-		log.Println("checking for updates...")
-		if err := wf.CheckForUpdate(); err != nil {
-			wf.FatalError(err)
-		}
-		return
-	}
-
-	var query string
-	if args["<query>"] != nil {
-		query = args["<query>"].(string)
-	}
-
-	log.Printf("query=%s", query)
-
-	// Call self with "check" command if an update is due and a
-	// check job isn't already running.
-	if wf.UpdateCheckDue() && !aw.IsRunning(updateJobName) {
-		log.Println("running update check in background...")
-		cmd := exec.Command("./alfred-web-searches", "check")
-		if err := aw.RunInBackground(updateJobName, cmd); err != nil {
-			log.Printf("error starting update check: %s", err)
-		}
-	}
-
-	if query == "" { // Only show update status if query is empty
-		// Send update status to Alfred
-		if wf.UpdateAvailable() {
-			wf.NewItem("Update Available!").
-				Subtitle("↩ to install").
-				Autocomplete("workflow:update").
-				Valid(false).
-				Icon(iconAvailable)
-		}
-	}
-
-	links := parseCSV()
-
-	var re1 = regexp.MustCompile(`.: `)
-	var re2 = regexp.MustCompile(`(all)`)
-
-	for key, value := range links {
-		if strings.Contains(key, "r: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(redditIcon).Var("RECENT", re2.ReplaceAllString(value, `week`)).Subtitle("⌃ = Search past week")
-		} else if strings.Contains(key, "d: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(docIcon)
-		} else if strings.Contains(key, "g: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(githubIcon)
-		} else if strings.Contains(key, "s: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(stackIcon)
-		} else if strings.Contains(key, "f: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(forumsIcon)
-		} else if strings.Contains(key, "t: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(translateIcon)
-		} else {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key)
-		}
-	}
-
-	if query != "" {
-		wf.Filter(query)
-	}
-
-	wf.WarnEmpty("No matching items", "Try a different query?")
-	wf.SendFeedback()
-}
-
-// Parses CSV of links and arguments
-func parseCSV() map[string]string {
 	var err error
 
-	// load values from file to a hash map
-	f, err := os.Open("websites.csv")
+	cmd, err := app.Parse(wf.Args())
 	if err != nil {
-		panic(err)
+		wf.FatalError(err)
 	}
-	defer f.Close()
 
-	r := csv.NewReader(f)
+	switch cmd {
+	case searchCmd.FullCommand():
+		err = doSearch()
+	case updateCmd.FullCommand():
+		err = doUpdate()
+	default:
+		err = fmt.Errorf("Uknown command: %s", cmd)
+	}
 
-	records, err := r.ReadAll()
+	// Check for update
+	if err == nil && cmd != updateCmd.FullCommand() {
+		err = checkForUpdate()
+	}
+
 	if err != nil {
-		log.Fatal(err)
+		wf.FatalError(err)
 	}
-
-	// holds user's search arguments and an appropriate search URL
-	links := make(map[string]string)
-
-	for _, record := range records {
-		links[record[0]] = record[1]
-	}
-
-	return links
-
 }
 
 func main() {
---
+++ search.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"encoding/csv"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+// doSearch makes a search for websites and returns results to Alfred.
+func doSearch() error {
+	showUpdateStatus()
+
+	log.Printf("query=%s", query)
+
+	links := parseCSV()
+
+	var re1 = regexp.MustCompile(`.: `)
+	var re2 = regexp.MustCompile(`(all)`)
+
+	for key, value := range links {
+		if strings.Contains(key, "r: ") {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(redditIcon).Var("RECENT", re2.ReplaceAllString(value, `week`)).Subtitle("⌃ = Search past week")
+		} else if strings.Contains(key, "d: ") {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(docIcon)
+		} else if strings.Contains(key, "g: ") {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(githubIcon)
+		} else if strings.Contains(key, "s: ") {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(stackIcon)
+		} else if strings.Contains(key, "f: ") {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(forumsIcon)
+		} else if strings.Contains(key, "t: ") {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(translateIcon)
+		} else {
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key)
+		}
+	}
+
+	if query != "" {
+		wf.Filter(query)
+	}
+
+	wf.WarnEmpty("No matching items", "Try a different query?")
+	wf.SendFeedback()
+	return nil
+}
+
+// parseCSV parses CSV for links and arguments.
+func parseCSV() map[string]string {
+	var err error
+
+	// Load values from file to a hash map
+	f, err := os.Open("websites.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Holds user's search arguments and an appropriate search URL
+	links := make(map[string]string)
+
+	for _, record := range records {
+		links[record[0]] = record[1]
+	}
+
+	return links
+
+}
