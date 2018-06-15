b16253637d09cb2403b7bdbc911e202ceb0d5d0f
Nikita Voloboev <nikita.voloboev@icloud.com>
2018-06-15 23:22:46 +0200

    refactor

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/alecthomas/kingpin"
 	"github.com/deanishe/awgo"
 	"github.com/deanishe/awgo/update"
 )
@@ -18,13 +15,6 @@ var (
 	stackIcon       = &aw.Icon{Value: "icons/stack.png"}
 	docIcon         = &aw.Icon{Value: "icons/doc.png"}
 
-	// Kingpin and script options
-	app *kingpin.Application
-
-	// Application commands
-	searchCmd *kingpin.CmdClause
-	updateCmd *kingpin.CmdClause
-
 	query string
 
 	repo = "nikitavoloboev/alfred-web-searches"
@@ -36,48 +26,10 @@ var (
 // Mostly sets up kingpin commands
 func init() {
 	wf = aw.New(update.GitHub(repo), aw.HelpURL(repo+"/issues"))
-
-	app = kingpin.New("ask", "Search websites.")
-
-	// Update command
-	updateCmd = app.Command("update", "Check for new version.").Alias("u")
-
-	// Commands using query
-	searchCmd = app.Command("search", "Search websites.").Alias("s")
-
-	// Common options
-	for _, cmd := range []*kingpin.CmdClause{
-		searchCmd,
-	} {
-		cmd.Flag("query", "Search query.").Short('q').StringVar(&query)
-	}
 }
 
 func run() {
-	var err error
-
-	cmd, err := app.Parse(wf.Args())
-	if err != nil {
-		wf.FatalError(err)
-	}
-
-	switch cmd {
-	case searchCmd.FullCommand():
-		err = doSearch()
-	case updateCmd.FullCommand():
-		err = doUpdate()
-	default:
-		err = fmt.Errorf("Uknown command: %s", cmd)
-	}
-
-	// Check for update
-	if err == nil && cmd != updateCmd.FullCommand() {
-		err = checkForUpdate()
-	}
-
-	if err != nil {
-		wf.FatalError(err)
-	}
+	doSearch()
 }
 
 func main() {
--- search.go
+++ search.go
@@ -3,12 +3,54 @@ package main
 import (
 	"encoding/csv"
 	"log"
+	"net/http"
 	"os"
 	"regexp"
 	"strings"
 )
 
-// doSearch makes a search for websites and returns results to Alfred.
+// TODO:
+func updateList() {
+	// Get websites list from GitHub
+	res, err := http.Get("https://raw.githubusercontent.com/nikitavoloboev/alfred-web-searches/master/workflow/websites.csv")
+	if err != nil {
+		return
+	}
+
+	defer res.Body.Close()
+	// body, err := ioutil.ReadAll(res.Body)
+	// TODO: Save it in Alfred dir
+}
+
+// parseCSV parses CSV for links and arguments.
+func parseCSV() map[string]string {
+	var err error
+
+	// Load file
+	f, err := os.Open("websites.csv")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	r := csv.NewReader(f)
+
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Holds user's search arguments and an appropriate search URL
+	links := make(map[string]string)
+
+	for _, record := range records {
+		links[record[0]] = record[1]
+	}
+
+	return links
+}
+
+// doSearch searches through the websites and returns results to Alfred.
 func doSearch() error {
 	showUpdateStatus()
 
@@ -21,7 +63,7 @@ func doSearch() error {
 
 	for key, value := range links {
 		if strings.Contains(key, "r: ") {
-			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(redditIcon).Var("RECENT", re2.ReplaceAllString(value, `week`)).Subtitle("⌃ = Search past week")
+			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(redditIcon).Var("RECENT", re2.ReplaceAllString(value, `week`)).Subtitle("⌘ = Search past week")
 		} else if strings.Contains(key, "d: ") {
 			wf.NewItem(key).Valid(true).Var("URL", value).Var("ARG", re1.ReplaceAllString(key, ``)).UID(key).Icon(docIcon)
 		} else if strings.Contains(key, "g: ") {
@@ -44,33 +86,4 @@ func doSearch() error {
 	wf.WarnEmpty("No matching items", "Try a different query?")
 	wf.SendFeedback()
 	return nil
-}
-
-// parseCSV parses CSV for links and arguments.
-func parseCSV() map[string]string {
-	var err error
-
-	// Load values from file to a hash map
-	f, err := os.Open("websites.csv")
-	if err != nil {
-		panic(err)
-	}
-	defer f.Close()
-
-	r := csv.NewReader(f)
-
-	records, err := r.ReadAll()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Holds user's search arguments and an appropriate search URL
-	links := make(map[string]string)
-
-	for _, record := range records {
-		links[record[0]] = record[1]
-	}
-
-	return links
-
 }
